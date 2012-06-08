0acd9eb9d84bcc465f8aa49215bc1d8a4d6a0677
Christopher H. Casebeer <christopher@chc.name>
2012-06-08 00:18:46 -0400

    Add override for broken callback after timeout
    Sencha's ScriptTagProxy calls the request's callback with a null operation
    after a timeout. This differs from the usual ServerProxy error behavior
    of calling the usual callback with a valid operation, and is separate
    code because a JSONP script tag embed can only fail by timemout, so must
    be actively checked.
    
    Fix to call the operation's callback.

--- web/js/overrides.js
+++ web/js/overrides.js
@@ -12,8 +12,9 @@ Ext.override(Ext.data.Store, {
      * Called internally when a Proxy has completed a load request
      */
     onProxyLoad: function(operation) {
+		console.log("In onProxyLoad, call was " + (operation.wasSuccessful() ? "":"NOT ") + "successful");
         var records = operation.getRecords();
-        
+
         if (operation.wasSuccessful()) {
             this.loadRecords(records, operation.addRecords);
         }
@@ -27,9 +28,26 @@ Ext.override(Ext.data.Store, {
         //this is a callback that would have been passed to the 'read' function and is optional
         var callback = operation.callback;
         
+		console.log("Callback type is " + typeof callback);
+		console.log(operation);
         if (typeof callback === 'function') {
+			console.log("calling callback");
             callback.call(operation.scope || this, records, operation, operation.wasSuccessful());
         }
     }
 
+});
+
+Ext.override(Ext.data.ScriptTagProxy, {
+    createTimeoutHandler: function(request, operation) {
+        this.afterRequest(request, false);
+
+        this.fireEvent('exception', this, request, operation);
+        
+		console.log("Creating callback handler; request.callback is " + typeof request.callback);
+		console.log(request);
+        if (typeof request.operation.callback == 'function') {
+            request.operation.callback.call(request.scope || window, operation, request.options, false);
+        }        
+    }	
 });
